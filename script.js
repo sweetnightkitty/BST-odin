f11dcbbcde671aca27cf5f82b6df4e765d02eda1
SaraToth <saratoth101@gmail.com>
2025-01-07 07:47:07 +0900

    Encapsulated algorithm that builds the binary search tree within the function buildTree, thus converting function tree into a factory function

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ function node (data) {
     };
 }
 
-function tree (myArray, start = 0, end = myArray.length - 1) {
-    //Base case, when array is empty.
-    if(start > end) {
-        return;
+function tree () {
+
+    function buildTree(myArray, start = 0, end = myArray.length - 1) {
+
+        //Base case, when array is empty.
+        if(start > end) {
+            return;
+        }
+        const mid = (end + start) / 2; // this value is equal to an array location
+        const root = node(myArray[mid]); // myArray at mid, is the value to be assigned as data
+
+        //root.leftChild needs to = left subArray
+        //root.rightChild needs to = right subArray
+
+        //recursively tree(root.leftChild);
+        //recursively tree(root.rightChild);
+
+        //return root
     }
-    const mid = (end + start) / 2; // this value is equal to an array location
-    const root = node(myArray[mid]); // myArray at mid, is the value to be assigned as data
 
-    //root.leftChild needs to = left subArray
-    //root.rightChild needs to = right subArray
-
-    //recursively tree(root.leftChild);
-    //recursively tree(root.rightChild);
-
-    //return root
 }
