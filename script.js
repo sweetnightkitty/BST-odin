68e8390f04d8d4e4a0f4a01f06b1a82bf849a7f4
SaraToth <saratoth101@gmail.com>
2025-01-07 13:43:13 +0900

    Change parameter and variable myArray to arr for easier readability

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function node (data) {
 
 function tree () {
 
-    function buildTree(myArray, start = 0, end = (myArray.length - 1)) {
+    function buildTree(arr, start = 0, end = (arr.length - 1)) {
 
         //Base case, when subarray becomes empty.
         if(start > end) {
@@ -18,12 +18,12 @@ function tree () {
         //Mid is the array index of the root data.
         const mid = Math.floor((end + start) / 2);
 
-        //myArray at mid is the value to be assigned as data
-        const root = node(myArray[mid]);
+        //arr at mid is the value to be assigned as data
+        const root = node(arr[mid]);
 
         //Recursively calculates mid and root of left and right subarrays and adds them (balanced) to the tree.
-        root.leftChild = buildTree(myArray.slice(0, (mid)));
-        root.rightChild = buildTree(myArray.slice(mid + 1));
+        root.leftChild = buildTree(arr.slice(0, (mid)));
+        root.rightChild = buildTree(arr.slice(mid + 1));
 
         return root
     }
