2db52b3a1ea3dfa9a3ba4c49daa717bef84e0476
SaraToth <saratoth101@gmail.com>
2025-01-08 14:59:23 +0900

    Refactor factory function so that encapsulated buildTree function is defined in the return object and provide driving code to test

--- script.js
+++ script.js
@@ -7,42 +7,36 @@ function node (data) {
 }
 
 function tree () {
-
-    function buildTree(arr, start = 0, end = (arr.length - 1)) {
-
-        //Base case, when subarray becomes empty.
-        if(start > end) {
-            return null;
-        }
-
-        //Sorts the array numerically
-        arr = [...new Set(arr)].sort((a, b) => {return a - b});
-
-        //Mid is the array index of the root data.
-        const mid = Math.floor((end + start) / 2);
-
-        //arr at mid is the value to be assigned as data
-        const root = node(arr[mid]);
-
-        //Recursively calculates mid and root of left and right subarrays and adds them (balanced) to the tree.
-        root.leftChild = buildTree(arr.slice(0, (mid)));
-        //mid + 1 -> ensures that the root is EXCLUDED in the rightChild
-        root.rightChild = buildTree(arr.slice(mid + 1));
-
-        return root
-    }
-
-
     return {
-        buildTree: buildTree,
+        buildTree: function (arr, start = 0, end = (arr.length - 1)) {
+            //Base case, when subarray becomes empty.
+            if(start > end) {
+                return null;
+            }
+
+            //Sorts the array numerically
+            arr = [...new Set(arr)].sort((a, b) => {return a - b});
+
+            //Mid is the array index of the root data.
+            const mid = Math.floor((end + start) / 2);
+
+            //arr at mid is the value to be assigned as data
+            const root = node(arr[mid]);
+
+            //Recursively calculates mid and root of left and right subarrays and adds them (balanced) to the tree.
+            root.leftChild = this.buildTree(arr.slice(0, (mid)));
+            //mid + 1 -> ensures that the root is EXCLUDED in the rightChild
+            root.rightChild = this.buildTree(arr.slice(mid + 1));
+
+            return root;
+        },
+
     }
 
 }
 
-const testArray = [5, 6, 7, 7, 8];
-const testTree = tree();
+//Driving Code
 const unsortedArray = [2, 1, 9, 4, 5, 3, 5];
 
-//Displays resulting test tree
-console.log(testTree.buildTree(testArray));
-console.log(testTree.buildTree(unsortedArray));
+const test = tree();
+console.log(test.buildTree(unsortedArray));
